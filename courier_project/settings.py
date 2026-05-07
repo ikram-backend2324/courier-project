42a40ff2b778a28b47be6dbcb94c602459975cc0
ikram-backend2324 <ikramxiyasov2@gmail.com>
2026-05-08 01:00:25 +0500

    inital commit

--- courier_project/settings.py
+++ courier_project/settings.py
@@ -80,7 +80,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY', '')
-OPENROUTER_MODEL = 'meta-llama/llama-3.2-3b-instruct:free'
+# OPENROUTER_MODEL = 'meta-llama/llama-3.2-3b-instruct:free'
+
+OPENROUTER_MODEL = 'microsoft/phi-3-mini-128k-instruct:free'
 
 JAZZMIN_SETTINGS = {
     "site_title": "Courier Route Admin",
